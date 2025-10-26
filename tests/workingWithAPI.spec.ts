2f4bbe94bebad33b245bdcbc8ebe63ff75f83bbf
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-26 20:39:23 +0200

    Modify API Response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -10,6 +10,17 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.route('*/**/api/articles*', async route => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = "This is a test title";
+    responseBody.articles[0].description = "This is a description";
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    });
+  })
+
   await page.goto('https://conduit.bondaracademy.com/');
   await page.waitForLoadState('networkidle');
 
@@ -18,4 +29,9 @@ test.beforeEach(async ({ page }) => {
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText("conduit");
+});
+
+test('modify the first article', async ({ page }) => {
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title');
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a description');
 });
