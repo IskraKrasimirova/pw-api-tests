19c653c36369310682dd4d36512db1456792a86e
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-26 20:25:48 +0200

    Mocking API

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
-  // Navigate to the starting URL before each test.
+  await page.route('*/**/api/tags', async route => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify(tags),
+    });
+  });
+
   await page.goto('https://conduit.bondaracademy.com/');
+  await page.waitForLoadState('networkidle');
+
+  await expect(page.locator('.tag-list .tag-pill', { hasText: 'Playwright' })).toHaveCount(1);
 });
 
 test('has title', async ({ page }) => {
