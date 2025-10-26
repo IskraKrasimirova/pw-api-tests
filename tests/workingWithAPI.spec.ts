50e85c449564d8526e206337864c47cf874ef2b5
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-26 22:56:01 +0200

    Delete article

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -10,21 +10,15 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
-  await page.route('*/**/api/articles*', async route => {
-    const response = await route.fetch();
-    const responseBody = await response.json();
-    responseBody.articles[0].title = "This is a test title";
-    responseBody.articles[0].description = "This is a description";
-
-    await route.fulfill({
-      body: JSON.stringify(responseBody)
-    });
-  })
-
   await page.goto('https://conduit.bondaracademy.com/');
   await page.waitForLoadState('networkidle');
 
   await expect(page.locator('.tag-list .tag-pill', { hasText: 'Playwright' })).toHaveCount(1);
+
+  await page.getByText('Sign in').click();
+  await page.getByRole('textbox', { name: "Email" }).fill('iskra@iskra.com');
+  await page.getByRole('textbox', { name: "Password" }).fill('Secret123');
+  await page.getByRole('button').click();
 });
 
 test('has title', async ({ page }) => {
@@ -32,6 +26,52 @@ test('has title', async ({ page }) => {
 });
 
 test('modify the first article', async ({ page }) => {
-  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title');
-  await expect(page.locator('app-article-list p').first()).toContainText('This is a description');
+  await page.route('*/**/api/articles*', async route => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = "This is a MOCK test title";
+    responseBody.articles[0].description = "This is a MOCK test description";
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    });
+  });
+
+  await page.getByText('Global Feed').click();
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title');
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK test description');
+});
+
+test('delete article', async ({ page, request }) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      "user": { "email": "iskra@iskra.com", "password": "Secret123" }
+    }
+  });
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data: {
+      "article": { "title": "This is a test title", "description": "This is a test description", "body": "This is a test body", "tagList": [] }
+    },
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+
+  });
+
+  expect(articleResponse.status()).toEqual(201);
+
+  await page.getByText('Global Feed').click();
+  await page.getByText('This is a test title').click();
+  await page.getByRole('button', { name: "Delete Article" }).first().click();
+  await page.getByText('Global Feed').click();
+
+   await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test title');
+
+   await page.getByText('Your Feed').click();
+
+   await expect(page.locator('app-article-list')).toContainText('No articles are here... yet.');
 });
