0fd34091a4efd33106431721aff35bcfbaaf87ab
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-27 19:44:11 +0200

    Create article

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -59,7 +59,6 @@ test('delete article', async ({ page, request }) => {
     headers: {
       Authorization: `Token ${accessToken}`
     }
-
   });
 
   expect(articleResponse.status()).toEqual(201);
@@ -69,9 +68,47 @@ test('delete article', async ({ page, request }) => {
   await page.getByRole('button', { name: "Delete Article" }).first().click();
   await page.getByText('Global Feed').click();
 
-   await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test title');
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test title');
 
-   await page.getByText('Your Feed').click();
+  await page.getByText('Your Feed').click();
 
-   await expect(page.locator('app-article-list')).toContainText('No articles are here... yet.');
+  await expect(page.locator('app-article-list')).toContainText('No articles are here... yet.');
+});
+
+test('create article', async ({ page, request }) => {
+  await page.getByText('New Article').click();
+  await page.getByRole('textbox', { name: 'Article Title' }).fill('Playwright tets');
+  await page.getByRole('textbox', { name: 'What\'s this article about?' }).fill('About Playwright');
+  await page.getByRole('textbox', { name: 'Write your article (in markdown)' }).fill('We like to use Playwright for automation');
+  await page.getByRole('button', { name: 'Publish Article' }).click();
+  const articleResponse = await page.waitForResponse('https://conduit-api.bondaracademy.com/api/articles/');
+  const articleResponseBody = await articleResponse.json();
+  const slugId = articleResponseBody.article.slug
+
+  await expect(page.locator('.article-page h1')).toContainText('Playwright tets');
+
+  await page.getByText('Home').click();
+  await page.getByText('Global Feed').click();
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('Playwright tets');
+  await expect(page.locator('app-article-list p').first()).toContainText('About Playwright');
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      "user": { "email": "iskra@iskra.com", "password": "Secret123" }
+    }
+  });
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  });
+
+  expect(deleteArticleResponse.status()).toEqual(204);
+
+  await page.getByText('Global Feed').click();
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('Playwright tets');
 });
